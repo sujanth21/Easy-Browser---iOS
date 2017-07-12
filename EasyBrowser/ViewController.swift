97721e4ed7e027cf6f51a7c40b10339158098334
Sujanth <sujanth1987@gmail.com>
2017-07-12 21:27:08 +1000

    Web site selections added

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -6,13 +6,49 @@
 //  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
 //
 
+import WebKit
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+    func openTapped() {
+        
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+        
+    }
+    
+    func openPage(action: UIAlertAction) {
+        
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url:url))
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
     override func didReceiveMemoryWarning() {
